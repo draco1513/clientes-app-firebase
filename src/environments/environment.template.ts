cc9d0dc9c6dfa7c4207d96b11e3bd28aced3ba04
David Mendoza <51796955+draco1513@users.noreply.github.com>
2026-07-15 02:37:32 -0500

    agregar template

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_PROYECTO.firebaseapp.com",
+    projectId: "TU_PROYECTO",
+    storageBucket: "TU_PROYECTO.appspot.com",
+    messagingSenderId: "...",
+    appId: "...",
+  },
+};
